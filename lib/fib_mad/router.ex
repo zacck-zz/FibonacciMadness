64dd1f97e7f17e9cfcd10b76a5264146376257ad
Zacck Osiemo <coderv63@gmail.com>
2019-04-09 16:33:45 +0200

    implement simple router to handle get calls

--- lib/fib_mad/router.ex
+++ lib/fib_mad/router.ex
@@ -9,5 +9,39 @@ defmodule FibMad.Router do
   plug(:dispatch)
 
   get("/", do: send_resp(conn, 200, "Welcome to Fibonacci"))
+
+  get("/sum/:number") do
+    {num, _} = Integer.parse(number)
+    {:ok, total} = Fibonacci.calculate(num)
+
+    send_resp(conn, 200, Jason.encode!(%{sum: total}))
+  end
+
+  get("/sums/:numbers") do
+    {:ok, totals} =
+      String.split(numbers, ",")
+      |> Enum.map(fn item ->
+        {num, _} = Integer.parse(item)
+        num
+      end)
+      |> Fibonacci.calculate()
+
+    send_resp(conn, 200, Jason.encode!(%{sums: totals}))
+  end
+
+  get("/history") do
+    history =
+      Fibonacci.history()
+      |> Enum.into(%{})
+
+    send_resp(conn, 200, Jason.encode!(%{history: history}))
+  end
+
+  get("/count") do
+    count = Fibonacci.history_count()
+
+    send_resp(conn, 200, Jason.encode!(%{counts: count}))
+  end
+
   match(_, do: send_resp(conn, 404, "This server is not capable of that"))
 end
