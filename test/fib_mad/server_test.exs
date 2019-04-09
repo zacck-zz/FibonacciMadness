ffcaae50edbb02fac9bf6714eaf9059272ab2d54
Zacck Osiemo <coderv63@gmail.com>
2019-04-09 14:55:50 +0200

    test that sums are retrieved historically

--- test/fib_mad/server_test.exs
+++ test/fib_mad/server_test.exs
@@ -4,6 +4,17 @@ defmodule FibonacciTest do
   """
   use ExUnit.Case
 
+  # capture log when testing
+  @moduletag capture_log: true
+
+  # restart application to ensure state is fresh for testing
+  setup do
+    Application.stop(:fib_mad)
+    Application.start(:fib_mad)
+
+    :ok
+  end
+
   describe "Fibonacci tests" do
     test "calculate/1 returns correct results for single number" do
       assert {:ok, 0} = Fibonacci.calculate(0)
@@ -13,6 +24,16 @@ defmodule FibonacciTest do
 
     test "calculate/1 returns correct results for a list of numbers" do
       assert {:ok, [0, 4]} = Fibonacci.calculate([0, 3])
+    end
+
+    test "server stores ordered history of calculations" do
+      Fibonacci.calculate(0)
+
+      Fibonacci.calculate(3)
+
+      Fibonacci.calculate(0)
+
+      assert {:ok, [{0, 0}, {3, 4}, {0, 0}]} = Fibonacci.history()
     end
   end
 end
